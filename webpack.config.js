fb8afd0593b9609a3950adad80edcecba805c891
Unknown <longtth85@gmail.com>
2018-11-01 01:24:51 +0100

    Added code for thumbnail pictures and removed webpack

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: "./public/javascripts/entry.js",
-    output: {
-        path: __dirname + "/public/dist",
-        filename: "bundle.js"
-    },
-    mode: "production"
-};
